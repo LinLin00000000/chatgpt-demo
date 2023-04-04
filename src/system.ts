3e940cb40ad7d6832f62d9f759029249a8ffd197
Lin <i@linlin.me>
2023-04-04 14:22:32 +0800

    fix: merge error

--- src/system.ts
+++ src/system.ts
@@ -10,7 +10,7 @@ export const defaultSetting = {
   model: "gpt-3.5-turbo" as Model
 }
 
-export const message = `Powered by OpenAI Vercel
+export const defaultMessage = `Powered by OpenAI Vercel
 喵。
 [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
